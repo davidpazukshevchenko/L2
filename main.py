9b1402136ecfb83797d5339d5a399c2a7db545da
davidpazukshevchenko <useplay132010@gmail.com>
2023-10-29 11:00:32 +0300

    new Student Class

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-class Test:
-    a = 1
-    b = 1
+class Student:
+    def __init__(self,name ="Vasya",age =18):
+        self.name = name
+        self.age = age
 
-test1 = Test()
-test2 = Test()
+    def say_hi(self):
+        print(f"Hello my name is {self.name} and I am {self.age} old!")
 
-test1.a = 25
 
-print(test1.a)
-print(test2.a)
+first_student = Student("Petya",16)
+second_student = Student("Anna",21)
+third_student = Student()
+
+
+first_student.say_hi()
+second_student.say_hi()
+third_student.say_hi()
